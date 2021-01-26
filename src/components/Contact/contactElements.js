23f9eaee6c47388f0d747737e6b0b057e03f9923
Raymond Robinson <rayissach@gmail.com>
2021-01-25 19:54:13 -0600

    Contact form textfields

--- src/components/Contact/contactElements.js
+++ src/components/Contact/contactElements.js
@@ -4,6 +4,19 @@ export const ContactContainer = styled.div `
 background: grey;
 height: 50vh;
 `
+export const ContactWrapper =styled.div `
+border: 1px black;
+// height: 75%;
+width: 50%;
+display: block;
+position: absolute;
+margin: auto;
+text-decoration: none;
+`
+
 export const ContactForm = styled.form `
-height: 90%;
+position: relative;
+justify-content: space-evenly;
+margin: auto;
+
 `
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { ContactContainer, ContactForm } from './contactElements.js';
+import { PictureInPictureAlt } from 'styled-icons/material';
+import { ContactContainer, ContactForm, ContactWrapper } from './contactElements.js';
 
+const h1Style = {
+  color: 'yellow',
+}
+const inputStyle = {
+  height: '20%',
+}
 const Contact = () => {
   return (
     <>
     <ContactContainer>
-      <ContactForm>
-
-      </ContactForm>
+      <ContactWrapper>
+        <ContactForm>
+          <h1 style= {h1Style}>Contact Me</h1>
+          <input type='text' id='name' style= {inputStyle} placeholder='Name' />
+          <br />
+          <input type='text' id='email' style= {inputStyle} placeholder='Email' />
+          <br />
+          <input type='text' id='subject' style= {inputStyle}name='subject' placeholder='Subject' />
+          <br />
+          <textarea type='text' id='Message' style= {inputStyle} placeholder='Message' />
+        </ContactForm>
+      </ContactWrapper> 
     </ContactContainer>
     </>
   )
